4445d6f2cea3e35c849389985c2fccecf4fe4de5
Sergey Tarabukin <sergey.tarabukin@gmail.com>
2026-06-18 11:04:39 +0500

    Fix deploy script

--- scripts/update-manifest.js
+++ scripts/update-manifest.js
@@ -5,12 +5,12 @@ const manifestPath = path.join(process.cwd(), 'dist', 'manifest.json');
 
 function getVersionString() {
     const now = new Date();
-    const yy = String(now.getFullYear()).slice(-2);
+    const yyyy = String(now.getFullYear());
     const mm = String(now.getMonth() + 1).padStart(2, '0');
     const dd = String(now.getDate()).padStart(2, '0');
     const hh = String(now.getHours()).padStart(2, '0');
     const min = String(now.getMinutes()).padStart(2, '0');
-    return `${yy}${mm}.${dd}.${hh}${min}`;
+    return `${yyyy}.${mm}${dd}.${hh}${min}`;
 }
 
 function updateManifest() {
